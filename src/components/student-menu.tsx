941d7b929ca5bcab6da94a15fb41e738a6fb6943
sads4ndwich <caiocamargo007@gmail.com>
2023-05-01 13:51:43 -0300

    feat(component): added student menu component

---
+++ src/components/student-menu.tsx
@@ -0,0 +1,68 @@
+import { CalendarDays, Home, Inbox, LogOut, Puzzle, Star } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+export function StudentMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/SadS4ndWiCh.png"
+            alt="@SadS4ndWiCh"
+          />
+          <AvatarFallback>SW</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            <p className="font-medium">John Doe</p>
+            <p className="w-[200px] truncate text-sm text-muted-foreground">
+              example.mail@fatec.sp.gov.br
+            </p>
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Home className="w-4 h-4 mr-2" />
+            <span>Home</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <CalendarDays className="w-4 h-4 mr-2" />
+            <span>Horários</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Star className="w-4 h-4 mr-2" />
+            <span>Notas Parciais</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Puzzle className="w-4 h-4 mr-2" />
+            <span>Faltas Pariciais</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Inbox className="w-4 h-4 mr-2" />
+            <span>Histórico</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem clasName="flex items-center cursor-pointer">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
