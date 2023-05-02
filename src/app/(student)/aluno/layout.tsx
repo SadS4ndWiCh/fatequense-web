2c3d488915ad58217df86eb4322da9b4fa8767e0
sads4ndwich <caiocamargo007@gmail.com>
2023-05-02 17:10:59 -0300

    feat(page): added student pages layout

---
+++ src/app/(student)/aluno/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { Sidebar } from "~/components/sidebar";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function StudentLayout({ children }: Props) {
+  return (
+    <div className="flex gap-2 min-h-screen">
+      <Sidebar />
+
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+}
