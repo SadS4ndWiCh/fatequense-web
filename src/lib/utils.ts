8de6e385ccc8de8c1742c5aa11461897e5cbaac1
sads4ndwich <caiocamargo007@gmail.com>
2023-05-02 21:33:58 -0300

    feat(utils): added tabulate schedule from api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,43 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
+
+import { Lesson, Schedules, schedulesSchema } from "./validators/schedule";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function dateToTimeStr(date: string) {
+  return dayjs(date).format("HH:mm");
+}
+
+type ScheduleHorary = Lesson & {
+  weekday: number;
+};
+
+export function tabulateSchedule(schedules: Schedules) {
+  const schedulesParsed = schedulesSchema.parse(schedules);
+
+  const horariesMap = new Map<string, ScheduleHorary[]>();
+
+  schedulesParsed.forEach((schedule) => {
+    schedule.forEach((lesson) => {
+      const key = `${dateToTimeStr(lesson.startsAt)}-${dateToTimeStr(
+        lesson.endsAt
+      )}`;
+      const prevHoraries = horariesMap.get(key);
+      const lessonHorary: ScheduleHorary = {
+        ...lesson,
+        weekday: dayjs(lesson.startsAt).day(),
+      };
+
+      horariesMap.set(
+        key,
+        prevHoraries ? [...prevHoraries, lessonHorary] : [lessonHorary]
+      );
+    });
+  });
+
+  return horariesMap;
 }
