7186bfb7305deec2100771c1ec844cfe2f02aeba
sads4ndwich <caiocamargo007@gmail.com>
2023-05-04 20:12:45 -0300

    feat(validation): added partial absences validation

---
+++ src/lib/validators/partial-absences.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const lessonSchema = z.object({
+  title: z.string(),
+  date: z.string().datetime(),
+  presences: z.number(),
+  absences: z.number(),
+});
+
+export const partialAbsenceSchema = z.object({
+  cod: z.string(),
+  disciplineName: z.string(),
+  totalPresences: z.number(),
+  totalAbsences: z.number(),
+  lessons: z.array(lessonSchema),
+});
+
+export const partialAbsencesSchema = z.array(partialAbsenceSchema);
+
+export type Lesson = z.infer<typeof lessonSchema>;
+export type PartialAbsence = z.infer<typeof partialAbsenceSchema>;
+export type PartialAbsences = z.infer<typeof partialAbsencesSchema>;
