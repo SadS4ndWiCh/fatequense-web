f3217361ab66e73bd856d28ee268d5bb55fdd896
sads4ndwich <caiocamargo007@gmail.com>
2023-05-04 20:32:24 -0300

    feat(validation): added history validation

---
+++ src/lib/validators/history.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const disciplineHistorySchema = z.object({
+  cod: z.string(),
+  disciplineName: z.string(),
+  description: z.string(),
+  finalGrade: z.number(),
+  presenceFrequency: z.number(),
+  renunciationAt: z.string().datetime().nullish(),
+  isApproved: z.boolean(),
+});
+
+export const historySchema = z.array(disciplineHistorySchema);
+
+export type DisciplineHistory = z.infer<typeof disciplineHistorySchema>;
+export type History = z.infer<typeof historySchema>;
