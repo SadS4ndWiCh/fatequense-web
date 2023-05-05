f2143ecf8e9c42e5a7671d7dbf04ad8789cc0c5f
sads4ndwich <caiocamargo007@gmail.com>
2023-05-05 18:44:27 -0300

    feat(auth): get current user and pass to sidebar

--- src/app/(student)/aluno/layout.tsx
+++ src/app/(student)/aluno/layout.tsx
@@ -1,14 +1,23 @@
 import { ReactNode } from "react";
+import { notFound } from "next/navigation";
+
 import { Sidebar } from "~/components/sidebar";
+import { getCurrentUser } from "~/lib/session";
 
 type Props = {
   children: ReactNode;
 };
 
-export default function StudentLayout({ children }: Props) {
+export default async function StudentLayout({ children }: Props) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return notFound();
+  }
+
   return (
     <div className="flex gap-2 min-h-screen">
-      <Sidebar />
+      <Sidebar user={user} />
 
       <main className="flex-1 p-6 ml-[100px]">{children}</main>
     </div>
