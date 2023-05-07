af2d3ff93642dc136f95c8c8fd091d6461bf318e
sads4ndwich <caiocamargo007@gmail.com>
2023-05-07 11:25:30 -0300

    feat(component): separate the introduction header into a component to get student name

--- src/app/(student)/aluno/page.tsx
+++ src/app/(student)/aluno/page.tsx
@@ -1,3 +1,4 @@
+import { IntroductionHeader } from "~/components/introduction-header";
 import { StudentDetails } from "~/components/student-details";
 import { StudentTodaySchedule } from "~/components/student-today-schedule";
 
@@ -9,14 +10,8 @@ export default function Student() {
   return (
     <div className="flex h-full gap-6 divide-x">
       <section className="flex-1 space-y-6">
-        <header className="relative rounded-md bg-red-50 p-8">
-          <small className="text-sm text-red-600">Bem vindo,</small>
-          <h1 className="text-3xl font-bold">Caio Vinícius!</h1>
-          <p className="mt-2">
-            Todas suas inforamções do siga mais relevantes serão atualizadas
-            aqui
-          </p>
-        </header>
+        {/* @ts-expect-error Async Server Component */}
+        <IntroductionHeader />
 
         {/* @ts-expect-error Async Server Component */}
         <StudentDetails />
