ab607d5b3233c60bc927ebb3d3775a8b0f63dc53
sads4ndwich <caiocamargo007@gmail.com>
2023-05-09 19:40:57 -0300

    refactor(student-menu): set as client component

--- src/components/student-menu.tsx
+++ src/components/student-menu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Session } from "next-auth";
 import { CalendarDays, Home, Inbox, LogOut, Puzzle, Star } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -40,31 +42,31 @@ export function StudentMenu({ user }: Props) {
         <DropdownMenuGroup>
           <DropdownMenuItem asChild className="cursor-pointer">
             <Link href="/aluno">
-              <Home className="w-4 h-4 mr-2" />
+              <Home className="mr-2 h-4 w-4" />
               <span>Home</span>
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild className="cursor-pointer">
             <Link href="/aluno/horarios">
-              <CalendarDays className="w-4 h-4 mr-2" />
+              <CalendarDays className="mr-2 h-4 w-4" />
               <span>Horários</span>
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild className="cursor-pointer">
             <Link href="/aluno/notas-parciais">
-              <Star className="w-4 h-4 mr-2" />
+              <Star className="mr-2 h-4 w-4" />
               <span>Notas Parciais</span>
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild className="cursor-pointer">
             <Link href="/aluno/faltas-parciais">
-              <Puzzle className="w-4 h-4 mr-2" />
+              <Puzzle className="mr-2 h-4 w-4" />
               <span>Faltas Pariciais</span>
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem asChild className="cursor-pointer">
             <Link href="/aluno/historico">
-              <Inbox className="w-4 h-4 mr-2" />
+              <Inbox className="mr-2 h-4 w-4" />
               <span>Histórico</span>
             </Link>
           </DropdownMenuItem>
@@ -73,13 +75,13 @@ export function StudentMenu({ user }: Props) {
         <DropdownMenuSeparator />
 
         <DropdownMenuItem
-          className="flex items-center cursor-pointer"
+          className="flex cursor-pointer items-center"
           onSelect={(event) => {
             event.preventDefault();
             signOut({ callbackUrl: `${window.location.origin}/login` });
           }}
         >
-          <LogOut className="w-4 h-4 mr-2" />
+          <LogOut className="mr-2 h-4 w-4" />
           <span>Sair</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
