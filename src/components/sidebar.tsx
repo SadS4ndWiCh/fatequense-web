e91c9245e7ad5d2600d3a1459f65726961092449
sads4ndwich <caiocamargo007@gmail.com>
2023-05-09 19:40:26 -0300

    refactor(component): remove opened state

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,43 +1,32 @@
-"use client";
-
-import { Suspense, useState } from "react";
 import Link from "next/link";
 import type { Session } from "next-auth";
 import { GraduationCap } from "lucide-react";
 
 import { StudentMenu } from "./student-menu";
 import { studentConfig } from "~/config/student";
-import { cn } from "~/lib/utils";
 
 type Props = {
   user: Session["user"];
 };
 
 export function Sidebar({ user }: Props) {
-  const [open, setOpen] = useState<boolean>(false);
-
   return (
-    <aside
-      className={cn(
-        "fixed top-0 left-0 bottom-0 flex flex-col min-w-[100px] p-4 shadow-md transition-all",
-        {
-          "items-center": !open,
-        }
-      )}
-    >
+    <aside className="fixed bottom-0 left-0 top-0 flex min-w-[100px] flex-col items-center p-4 shadow-md transition-all">
       <section className="flex-1 space-y-6 divide-y">
-        <header className={cn("flex", { "justify-center": !open })}>
-          <div className="w-fit p-4 rounded-md bg-red-50">
-            <GraduationCap className="w-5 h-5 text-red-600" />
-          </div>
+        <header className="flex justify-center">
+          <Link href="/aluno">
+            <div className="w-fit rounded-md bg-red-50 p-4">
+              <GraduationCap className="h-5 w-5 text-red-600" />
+            </div>
+          </Link>
         </header>
 
         <nav className="flex flex-col items-center pt-6">
           {studentConfig.sidebar.map((link) => (
             <Link key={link.href} href={link.href} className="group w-full">
-              <div className="flex items-center gap-2 p-4 rounded-md group-hover:bg-zinc-50 group-hover:text-zinc-600">
-                <link.icon className="w-5 h-5" />
-                {open && <span>{link.label}</span>}
+              <div className="flex items-center gap-2 rounded-md p-4 group-hover:bg-zinc-50 group-hover:text-zinc-600">
+                <link.icon className="h-5 w-5" />
+                <span className="sr-only">{link.label}</span>
               </div>
             </Link>
           ))}
