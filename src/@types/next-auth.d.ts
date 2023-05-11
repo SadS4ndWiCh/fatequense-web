ffa4e919b65613da99a94e382226cacc4d2cb1e9
sads4ndwich <caiocamargo007@gmail.com>
2023-05-11 20:29:42 -0300

    type(next-auth): include student id `ra` in User attr

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      ra: string;
       name: string;
       email: string;
       picture: string;
@@ -17,6 +18,7 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface DefaultJWT {
+    ra: string;
     accessToken: string;
   }
 }
