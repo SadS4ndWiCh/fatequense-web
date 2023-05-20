7dc5b8b636b83a19b98848b5536aa60daa7bfd20
sads4ndwich <caiocamargo007@gmail.com>
2023-05-20 17:09:06 -0300

    feat(session): include get session function

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,6 +1,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth";
 
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
 
