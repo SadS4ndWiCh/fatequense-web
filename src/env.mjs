0111ebe0ad649c504d0960bf5853230978ce1452
sads4ndwich <caiocamargo007@gmail.com>
2023-05-20 13:50:15 -0300

    feat: added t3 env

---
+++ src/env.mjs
@@ -0,0 +1,24 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+	server: {
+		NEXTAUTH_URL: z.string().url().optional(),
+		NEXTAUTH_SECRET: z.string().min(1),
+
+		JWT_SECRET_KEY: z.string().min(1),
+		JWT_ALGORITHM: z.string().min(1)
+	},
+	client: {
+		NEXT_PUBLIC_API_BASE_URL: z.string().url().min(1),
+	},
+	runtimeEnv: {
+		NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+
+		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+		JWT_ALGORITHM: process.env.JWT_ALGORITHM,
+	}
+});
