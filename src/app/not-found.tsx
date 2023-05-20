fef7195e2de4ec4673494e8a0dacdc78df869f8e
sads4ndwich <caiocamargo007@gmail.com>
2023-05-20 17:02:53 -0300

    feat(not-found): added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+export default function NotFound() {
+	return (
+		<div className="w-screen min-h-screen grid place-items-center">
+			<div className="text-center">
+				<div className="mb-20">
+					<h2 className="relative text-3xl font-medium z-10 before:absolute before:content-['404'] before:text-9xl before:font-bold before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:text-zinc-100 before:-z-10">
+						Página não entrada
+					</h2>
+				</div>
+
+				<Link href='/login' className={cn(buttonVariants({ variant: 'ghost' }))}>
+					<ArrowLeft className="w-4 h-4 mr-2" />
+					<span>Voltar</span>
+				</Link>
+			</div>
+		</div>
+	)
+}
