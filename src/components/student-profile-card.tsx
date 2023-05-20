a76b0a50e9cd90a9011da7ca6b3c6da5acbe20b8
sads4ndwich <caiocamargo007@gmail.com>
2023-05-20 14:12:40 -0300

    feat(component): added student profile card

---
+++ src/components/student-profile-card.tsx
@@ -0,0 +1,46 @@
+import { Session } from "next-auth";
+
+import { CopyButton } from "~/components/copy-button";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+
+type Props = {
+  user: Session['user'];
+};
+
+export function StudentProfileCard({ user }: Props) {
+  return (
+    <section className="flex w-[265px] flex-col items-center rounded-md px-4 py-6 gap-2">
+      <Avatar className="h-32 w-32">
+        <AvatarImage src={user.picture} alt={user.name} />
+        <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+      </Avatar>
+
+      <div className="mt-2 w-full space-y-4">
+        <section>
+          <h2 className="truncate text-xl font-medium">{user.name}</h2>
+        </section>
+
+        <section className="flex flex-col gap-1">
+          <div className="flex-1">
+            <span className="text-xs">Email Institucional</span>
+            <CopyButton
+              id="institutional-email"
+              content={user.email}
+              className="w-full"
+            >
+              <span className="mr-2 truncate text-sm">
+                {user.email}
+              </span>
+            </CopyButton>
+          </div>
+          <div className="flex-1">
+            <span className="text-xs">RA</span>
+            <CopyButton id="student-id" content={user.ra} className="w-full">
+              <span className="mr-2 truncate text-sm">{user.ra}</span>
+            </CopyButton>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
