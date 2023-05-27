a38b90fe1833e14487d404380f23be88ff73fa2b
sads4ndwich <caiocamargo007@gmail.com>
2023-05-27 12:22:53 -0300

    feat: change not found 404 text color in dark mode

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
     <div className="grid min-h-screen w-screen place-items-center">
       <div className="text-center">
         <div className="mb-20">
-          <h2 className="relative z-10 text-3xl font-medium before:absolute before:left-1/2 before:top-1/2 before:-z-10 before:-translate-x-1/2 before:-translate-y-1/2 before:text-9xl before:font-bold before:text-zinc-100 before:content-['404']">
+          <h2 className="relative z-10 text-3xl font-medium before:absolute before:left-1/2 before:top-1/2 before:-z-10 before:-translate-x-1/2 before:-translate-y-1/2 before:text-9xl before:font-bold before:text-zinc-100 before:content-['404'] dark:before:text-gray-800">
             Página não entrada
           </h2>
         </div>
