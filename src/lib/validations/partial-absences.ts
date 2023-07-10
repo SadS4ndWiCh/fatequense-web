2347ba45ca869732295d0ac3407a8ad7d5cb362a
sads4ndwich <caiocamargo007@gmail.com>
2023-07-10 14:17:37 -0300

    refactor(validation): split discipline lesson schema from entire schema

--- src/lib/validations/partial-absences.ts
+++ src/lib/validations/partial-absences.ts
@@ -1,19 +1,21 @@
 import { z } from 'zod';
 
+export const disciplineLessonsSchema = z.array(
+  z.object({
+    title: z.string(),
+    date: z.string().nullable(),
+    presences: z.number(),
+    absences: z.number(),
+  }),
+)
+
 export const studentPartialAbsencesSchema = z.array(
   z.object({
     cod: z.string().length(6),
     disciplineName: z.string().min(1),
     totalPresences: z.number(),
     totalAbsences: z.number(),
-    lessons: z.array(
-      z.object({
-        title: z.string(),
-        date: z.string().nullable(),
-        presences: z.number(),
-        absences: z.number(),
-      }),
-    ),
+    lessons: disciplineLessonsSchema,
   }),
 );
 
