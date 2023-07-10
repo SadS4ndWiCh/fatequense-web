4125255fee229248571b29a3a08b3524a070c35d
sads4ndwich <caiocamargo007@gmail.com>
2023-07-10 14:17:03 -0300

    refactor(validation): split schedule lesson schema from entire schema

--- src/lib/validations/schedule.ts
+++ src/lib/validations/schedule.ts
@@ -1,20 +1,20 @@
 import { z } from 'zod';
 
+export const scheduleLesson = z.object({
+  cod: z.string().length(6),
+  discipline: z
+    .object({
+      name: z.string().min(1),
+      hoursPerLesson: z.number(),
+      teacherName: z.string().min(1),
+    })
+    .optional(),
+  startsAt: z.string().datetime(),
+  endsAt: z.string().datetime(),
+});
+
 export const studentScheduleSchema = z.array(
-  z.array(
-    z.object({
-      cod: z.string().length(6),
-      discipline: z
-        .object({
-          name: z.string().min(1),
-          hoursPerLesson: z.number(),
-          teacherName: z.string().min(1),
-        })
-        .optional(),
-      startsAt: z.string().datetime(),
-      endsAt: z.string().datetime(),
-    }),
-  ),
+  z.array(scheduleLesson),
 );
 
 export type Schedule = z.infer<typeof studentScheduleSchema>;
