94e6d61ce341cc50d070e0972007b22e5c92f2ef
sads4ndwich <caiocamargo007@gmail.com>
2023-07-10 14:19:16 -0300

    refactor(api): use zod fetch to type-safe fetch

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,35 @@
-import { env } from "../env.mjs";
-import { createFetch } from "./fetch";
+import { ZodFetcher, createZodFetcher } from "zod-fetch";
+import { env } from "~/env.mjs";
 
-export const api = createFetch({ baseURL: env.NEXT_PUBLIC_API_BASE_URL });
+type Methods = 'get' | 'post' | 'patch';
+
+const fetcher = createZodFetcher();
+
+export const api: Record<Methods, ZodFetcher<typeof fetch>> = {
+  get(schema, url, options) {
+    return fetcher(schema, new URL(url.toString(), env.NEXT_PUBLIC_API_BASE_URL), {
+      method: 'GET',
+      ...options
+    })
+  },
+  post(schema, url, options) {
+    return fetcher(schema, new URL(url.toString(), env.NEXT_PUBLIC_API_BASE_URL), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        ...options?.headers
+      },
+      ...options
+    })
+  },
+  patch(schema, url, options) {
+    return fetcher(schema, new URL(url.toString(), env.NEXT_PUBLIC_API_BASE_URL), {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        ...options?.headers
+      },
+      ...options
+    })
+  },
+}
