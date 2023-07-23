bc2ad55d2158b8cf4d8abf60fbaa0dd648cde8c0
sads4ndwich <caiocamargo007@gmail.com>
2023-07-23 15:10:49 -0300

    lint: lint files

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,14 +1,14 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes } from 'react'
 
-import { cn } from "~/lib/utils";
+import { cn } from '~/lib/utils'
 
 function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn('animate-pulse rounded-md bg-muted', className)}
       {...props}
     />
-  );
+  )
 }
 
-export { Skeleton };
+export { Skeleton }
