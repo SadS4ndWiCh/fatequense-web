826ee6cf7c984da89bd7d416258189acb99d3b0d
sads4ndwich <caiocamargo007@gmail.com>
2023-07-25 22:53:46 -0300

    feat: added nav for mobile

--- src/components/layouts/mobile-nav.tsx
+++ src/components/layouts/mobile-nav.tsx
@@ -4,10 +4,20 @@ import { GraduationCap, PanelRightClose, User } from 'lucide-react'
 
 import { studentConfig } from '~/config/student'
 
+import { SignOutButton } from '../sign-out-button'
+import { ThemeToggle } from '../theme-toggle'
 import { Button } from '../ui/button'
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '../ui/sheet'
+import { Separator } from '../ui/separator'
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '../ui/sheet'
 
-export function MobileNav() {
+export async function MobileNav() {
   return (
     <div className="md:hidden">
       <Sheet>
@@ -16,39 +26,45 @@ export function MobileNav() {
             <PanelRightClose className="w-5 h-5" />
           </Button>
         </SheetTrigger>
-        <SheetContent side="left">
-          <SheetTitle>
-            <Link href="/aluno">
-              <div className="w-fit rounded-md bg-red-50 p-4 dark:bg-red-500">
-                <GraduationCap className="h-5 w-5 text-red-600 dark:text-red-50" />
-              </div>
-            </Link>
-          </SheetTitle>
-
-          <div className="mt-4 space-y-2 divide-y">
-            <div>
-              <Link
-                href="/aluno/perfil"
-                className="w-full flex items-center gap-2 rounded-md p-4 hover:bg-zinc-50 hover:text-gray-600 dark:hover:bg-gray-800 dark:hover:text-zinc-50"
-              >
-                <User className="h-5 w-5" />
-                <span>Perfil</span>
+        <SheetContent side="left" className="flex flex-col">
+          <SheetHeader>
+            <SheetTitle>
+              <Link href="/aluno">
+                <div className="w-fit rounded-md bg-red-50 p-4 dark:bg-red-500">
+                  <GraduationCap className="h-5 w-5 text-red-600 dark:text-red-50" />
+                </div>
               </Link>
-            </div>
+            </SheetTitle>
+          </SheetHeader>
 
-            <div className="pt-2">
-              {studentConfig.sidebar.map((link) => (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  className="w-full flex items-center gap-2 rounded-md p-4 hover:bg-zinc-50 hover:text-gray-600 dark:hover:bg-gray-800 dark:hover:text-zinc-50"
-                >
-                  <link.icon className="h-5 w-5" />
-                  <span>{link.label}</span>
-                </Link>
-              ))}
-            </div>
+          <div className="flex-1 mt-4 space-y-1">
+            <Link
+              href="/aluno/perfil"
+              className="w-full flex items-center gap-2 rounded-md p-4 hover:bg-zinc-50 hover:text-slate-600 dark:hover:bg-slate-800 dark:hover:text-zinc-50"
+            >
+              <User className="h-5 w-5" />
+              <span>Perfil</span>
+            </Link>
+
+            {studentConfig.sidebar.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className="w-full flex items-center gap-2 rounded-md p-4 hover:bg-zinc-50 hover:text-slate-600 dark:hover:bg-slate-800 dark:hover:text-zinc-50"
+              >
+                <link.icon className="h-5 w-5" />
+                <span>{link.label}</span>
+              </Link>
+            ))}
           </div>
+
+          <SheetFooter className="gap-4 flex-col items-start">
+            <ThemeToggle />
+
+            <Separator />
+
+            <SignOutButton />
+          </SheetFooter>
         </SheetContent>
       </Sheet>
     </div>
