3db9be0a497f7050320b62f74f55af9ec060cb39
sads4ndwich <caiocamargo007@gmail.com>
2023-07-26 16:46:07 -0300

    feat: use separator component

--- src/app/(student)/aluno/page.tsx
+++ src/app/(student)/aluno/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation'
 import { getCurrentUser } from '~/lib/session'
 
 import { ScrollArea } from '~/components/ui/scroll-area'
+import { Separator } from '~/components/ui/separator'
 
 import { StudentSummary } from '~/components/student/student-summary'
 import { StudentTodaySchedule } from '~/components/student/student-today-schedule'
@@ -18,7 +19,7 @@ export default async function Student() {
   const name = user.name.split(' ').slice(0, 2).join(' ')
 
   return (
-    <div className="grid grid-cols-1 h-full lg:grid-cols-[1fr_min-content] lg:gap-6 lg:divide-x">
+    <div className="grid grid-cols-1 h-full lg:grid-cols-[1fr_min-content_min-content] lg:gap-3">
       <section className="flex-1 space-y-6">
         <div className="rounded-md bg-red-50 p-8 border-2 border-transparent dark:border-red-500 dark:bg-transparent">
           <small className="text-sm text-red-600 dark:text-red-500">
@@ -33,6 +34,9 @@ export default async function Student() {
 
         <StudentSummary />
       </section>
+
+      <Separator className="hidden lg:inline-block" orientation="vertical" />
+
       <ScrollArea className="hidden lg:block lg:max-h-[calc(100vh-3rem)]">
         <section className="h-full space-y-2 md:pl-6">
           <h2 className="text-lg font-bold">Aulas de Hoje</h2>
